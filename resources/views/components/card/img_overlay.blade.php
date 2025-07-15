baf2e81c974ad291f7f0a49775192eed4a4520cd
Osama Abdelrahman <osbusiness662@gmail.com>
2025-07-15 21:55:48 +0100

    Adding new card designs

---
+++ resources/views/components/card/img_overlay.blade.php
@@ -0,0 +1,6 @@
+<div class="card">
+    <img class="d-block w-100" src="{{asset('imgs/s24.png')}}" alt="Product_image">
+    <div class="card-img-overlay">
+        {{$slot}}
+    </div>
+</div>
---
+++ resources/views/components/card/normal.blade.php
@@ -0,0 +1,6 @@
+<div class="card border-light shadow-sm">
+    <img class="card-img aspect-ratio-1 bg-cover" src="{{asset('imgs/s24.png')}}" alt="Product_image">
+    <div class="card-body">
+        {{$slot}}
+    </div>
+</div>
